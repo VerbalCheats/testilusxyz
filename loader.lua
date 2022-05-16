5e7593055b60df105dade08777ea700275fa2f76
Jack <101001475+VerbalCheats@users.noreply.github.com>
2022-05-16 18:18:44 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,7 @@
 if shotgun then
     loadstring(game:HttpGetAsync("https://testilus.xyz/shotgun.lua"))()
 end
-if slasherkatana then
-    loadstring(game:HttpGetAsync("https://testilus.xyz/slasherkatana.lua"))()
+
+if bazooka then
+    loadstring(game:HttpGetAsync("https://testilus.xyz/bazooka.lua"))()
 end
