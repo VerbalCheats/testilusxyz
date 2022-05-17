988875b3e721f999c764e15f6e992e7b0ccd78bc
Jack <101001475+VerbalCheats@users.noreply.github.com>
2022-05-17 18:38:06 +0100

    Create orhub.lua

---
+++ orhub.lua
@@ -0,0 +1,203 @@
+print('Orhub loaded, made by Overlord#6666 and OrPlayz#0255 *https://discord.gg/QGThVHWzrM*')
+
+local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
+
+local X = Material.Load({
+	Title = "skidhub [cracked by testilus]",
+	Style = 3,
+	SizeX = 500,
+	SizeY = 350,
+	Theme = "Dark",
+	ColorOverrides = {
+		MainFrame = Color3.fromRGB(60,60,60)
+	}
+})
+
+local Main = X.New({
+	Title = "Main"
+})
+
+local Credits = X.New({
+	Title = "Credits"
+})
+
+local A = Main.Button({
+	Text = "Spinny Penis (NEW)",
+	Callback = function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/UgVQ0DhY", true))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "fortnite epic"
+			})
+		end
+	}
+})
+
+local A = Main.Button({
+	Text = "USC (Remade Edit)",
+	Callback = function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/VSb0yyvy", true))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "fortnite epic"
+			})
+		end
+	}
+})
+
+local A = Main.Button({
+	Text = "Hammer Smasher (Revamped)",
+	Callback = function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/bgYZz7bY", true))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "fortnite epic"
+			})
+		end
+	}
+})
+
+local A = Main.Button({
+	Text = "Levitating Sniper (NEW)",
+	Callback = function()
+		loadstring(game:HttpGet('https://pastebin.com/raw/cVq7CtyA'))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "fortnite epic"
+			})
+		end
+	}
+})
+
+
+
+
+local A = Main.Button({
+	Text = "Chair Disabler (Revamped)",
+	Callback = function()
+		loadstring(game:HttpGet('https://pastebin.com/raw/p9vrca6r'))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "fortnite epic"
+			})
+		end
+	}
+})
+
+local A = Main.Button({
+	Text = "Anti Fling (NEW)",
+	Callback = function()
+		loadstring(game:HttpGet('https://pastebin.com/raw/juQHG2mg'))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "fortnite epic"
+			})
+		end
+	}
+})
+local A = Main.Button({
+	Text = "Net Bypass (April, 2022)",
+	Callback = function()
+		loadstring(game:HttpGet('https://paste.ee/r/vXN7L'))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "fortnite epic"
+			})
+		end
+	}
+})
+local A = Main.Button({
+	Text = "Neptunian V (Torso Fling Edit)",
+	Callback = function()
+		loadstring(game:HttpGet('https://pastebin.com/raw/AMb014sw'))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "fortnite epic"
+			})
+		end
+	}
+})
+local A = Main.Button({
+	Text = "Neptunian V Switcher (Revamped)",
+	Callback = function()
+		loadstring(game:HttpGet('https://pastebin.com/raw/aZdqTEJG'))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "fortnite epic"
+			})
+		end
+	}
+})
+local A = Main.Button({
+	Text = "Leg Runner (R6)",
+	Callback = function()
+		loadstring(game:HttpGet('https://gist.githubusercontent.com/1BlueCat/7291747e9f093555573e027621f08d6e/raw/23b48f2463942befe19d81aa8a06e3222996242c/FE%2520Da%2520Feets'))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "fortnite epic"
+			})
+		end
+	}
+})
+
+local A = Credits.Button({
+	Text = "Overlord#6666",
+	Callback = function()
+		setclipboard("Overlord#6666")
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "fortnite epic"
+			})
+		end
+	}
+})
+
+local A = Credits.Button({
+	Text = "OrPlayz#0255",
+	Callback = function()
+		setclipboard("OrPlayz#0255")
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "fortnite epic"
+			})
+		end
+	}
+})
+
+local A = Credits.Button({
+	Text = "OrHub Discord (Click To Copy)",
+	Callback = function()
+		setclipboard("https://discord.gg/QGThVHWzrM")
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "fortnite epic"
+			})
+		end
+	}
+})
